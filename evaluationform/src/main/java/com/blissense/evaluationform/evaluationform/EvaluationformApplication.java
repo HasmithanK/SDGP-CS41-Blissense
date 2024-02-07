8b90a7cbd983ad17899f17b50babc2504e1e523d
HasmithanK <kumararajahasmithan@icloud.com>
2024-02-08 00:00:20 +0530

    added files to git

---
+++ evaluationform/src/main/java/com/blissense/evaluationform/evaluationform/EvaluationformApplication.java
@@ -0,0 +1,13 @@
+package com.blissense.evaluationform.evaluationform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationformApplication.class, args);
+	}
+
+}
---
+++ evaluationform/src/test/java/com/blissense/evaluationform/evaluationform/EvaluationformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissense.evaluationform.evaluationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
